bd675616e900a57817911957ea635331d739896b
baya laadjel <bata_cher@hotmail.fr>
2023-09-07 09:49:10 +0200

    add banner

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+
 import Header from "../components/header/header";
 import Footer from "../components/footer/footer";
+import Banner from "../components/banner/banner";
 
 export default function Home() {
+
+  const sourcebanner ="./src/assets/logo banner.png";
+  const titrebanner = "Chez vous, partout et ailleurs"
+
   return (
     <>
       <Header />
-      <h1> page d'accueil</h1>
+      <Banner src={sourcebanner} title ={titrebanner}/>
+      <h2> page d'accueil</h2>
       <Footer/>
     </>
   );
