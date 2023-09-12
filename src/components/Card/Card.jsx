7808a1c28979ebb1dadd47b464b61d98b1711e03
baya laadjel <bata_cher@hotmail.fr>
2023-09-12 09:42:57 +0200

    maj css

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom"
 import "./Card.css"
 
 export default function Card(props) {
   return (
-    <figure className="Card">
-        <img src={props.cover} alt= {props.title} />
-        <figcaption > {props.title} </figcaption>
-    </figure>
+    // <Link to={`/Logement/${props.id}`}>
+    <Link to={`/Logement/${props.id}`}>
+      <figure className="Card">
+          <img src={props.cover} alt= {props.title} />
+          <figcaption > {props.title} </figcaption>
+      </figure>
+    </Link>
   )
 }
